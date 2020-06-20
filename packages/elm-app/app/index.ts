a5f17005d7eadec239da818f9d7c709134d1e253
Anthonny <anthonny.querouil@gmail.com>
2020-06-20 19:31:16 +0200

    :construction: add linter configuration, checked with husky

--- packages/elm-app/app/index.ts
+++ packages/elm-app/app/index.ts
@@ -1,6 +1,15 @@
 import "./main.scss";
 const { Elm } = require("./src/Main.elm");
 
+/* 
+  We should add the definition of Flags
+  If you don't use Flags, remove this interface 
+  and change the definition of flags in Configuration to
+  flags: {}
+  or you can also remove flags and use a default value
+  in Elm.Main.init
+*/
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Flags {}
 
 export interface Configuration {
@@ -8,6 +17,11 @@ export interface Configuration {
   flags: Flags;
 }
 
+/* 
+  We should add the definition of Ports
+  If you don't use Ports, change the returned type to void
+*/
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface Ports {}
 
 export const init: (configuration: Configuration) => Ports = (
