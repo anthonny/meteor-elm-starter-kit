e88c28bae622575ee7f48f945919953e0b7e4842
Anthonny <anthonny.querouil@gmail.com>
2022-02-21 15:27:38 +0100

    update tailwind config

--- packages/elm-app/app/tailwind.config.js
+++ packages/elm-app/app/tailwind.config.js
@@ -9,27 +9,8 @@ module.exports = {
         extend: {
             colors: {
                 primary: colors.indigo,
-                gray: colors.slate,
-                orange: colors.orange,
-            },
-            fontFamily: {
-                sans: ["Inter", ...defaultTheme.fontFamily.sans],
-            },
-            typography: (theme) => ({
-                DEFAULT: {
-                    css: {
-                        code: {
-                            backgroundColor: colors.blue[50],
-                        },
-                        "code::before": {
-                            content: '""',
-                        },
-                        "code::after": {
-                            content: '""',
-                        },
-                    },
-                },
-            }),
+                gray: colors.slate
+            }
         },
     },
     variants: {
